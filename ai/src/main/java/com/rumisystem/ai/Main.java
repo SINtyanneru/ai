91800e2a3687b2df728a30d05db80845ef7b65ff
SINtyanneru <irjitgd@gmail.com>
2024-03-10 12:01:29 +0900

    次はログイン実装

---
+++ ai/src/main/java/com/rumisystem/ai/Main.java
@@ -0,0 +1,18 @@
+package com.rumisystem.ai;
+
+public class Main {
+	public static final String VERISON = "JAVA_0.1";
+
+	public static void main(String[] args) {
+		System.out.println("   __    ____  _____  ___ ");
+		System.out.println("  /__\\  (_  _)(  _  )/ __)");
+		System.out.println(" /(__)\\  _)(_  )(_)( \\__ \\");
+		System.out.println("(__)(__)(____)(_____)(___/\n");
+
+		System.out.println("Ai v" + VERISON);
+	}
+
+	private static void LOGIN(){
+
+	}
+}
